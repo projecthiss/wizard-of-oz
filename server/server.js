97ae272ad90c82fd1db26f79dbb33c56987e032e
CR <mail@corneliusreh.com>
2022-12-08 01:21:58 +0100

    changed

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+app.use(express.json(
+    {type: ['application/json', 'text/plain']}
+))
+
+const router = express.Router()
+
+//link to static client files (generated in production Mode)
+const staticFiles = express.static(path.join(__dirname, '../../client/build'))
+
+app.use(staticFiles)
+
+//bandit.setActions()
+
+app.use(router)
+
+app.use('*', staticFiles)
+
+app.set('port', (process.env.PORT || 3001))
+app.listen(app.get('port'), () => {
+    console.log(`Listening on ${app.get('port')}`)
+})
